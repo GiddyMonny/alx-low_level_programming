2c0ab5ebb0a9ee832458ca555ab46890ae2d0b42
Gideon Osei Acheampong <gideon.africa100@gmail.com>
2022-10-18 22:13:12 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Description: Prints the sum of even-valued
+ *
+ *    * Fibonacci sequence not exceed 4million
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+int main(void)
+
+{
+
+		long a = 1;
+
+			long b = 2;
+
+				int n = 3;
+
+
+
+					printf("%li, ", a);
+
+						printf("%li, ", b);
+
+
+
+							while (n < 50)
+
+									{
+
+												long fib = a + b;
+
+
+
+														printf("%li, ", fib);
+
+																a = b;
+
+																		b = fib;
+
+																				n++;
+
+																					}
+
+
+
+								while (n == 50)
+
+										{
+
+													long fib = a + b;
+
+
+
+															printf("%li\n", fib);
+
+																	a = b;
+
+																			b = fib;
+
+																					n++;
+
+																						}
+
+
+
+									return (0);
+
+}
